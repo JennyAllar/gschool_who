74ffbca60ca7880cc5d1634960646d427a708188
Jenny Allar <JennyAllar@gmail.com>
2014-06-19 13:40:06 -0600

    User can go to the new game page.

--- spec/features/welcome_spec.rb
+++ spec/features/welcome_spec.rb
@@ -6,4 +6,11 @@ feature 'welcome functions' do
     visit '/'
     expect(page).to have_content 'Welcome to gSchool Who?!'
   end
+  
+  scenario 'a user can start a new game' do
+
+    visit '/'
+    click_on 'Start a New Game!'
+    expect(page).to have_content 'Pick your gSchooler!'
+  end
 end
